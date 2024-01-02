0a39fe1b7a8d9aa0fe74d6afb0646d88146f97b2
Akash kumar <mail.akashofficial@gmail.com>
2024-01-02 10:06:33 +0530

    comm it

--- controller/orderMenuGroup_controller.js
+++ controller/orderMenuGroup_controller.js
@@ -8,19 +8,23 @@ exports.createGroup = [
     body('group_name').isLength({ min: 3, max: 50 }).withMessage(message.GROUP_NAME_MUST_BE_ATLEAST_3_CHARACTER),
     body('arabic_group_name').isLength({ min: 3, max: 50 }).withMessage(message.ARABIC_GROUP_NAME_MUST_NOT_EMPTY),
     async (req, res) => {
-        const result = validationResult(req);
-        if (!result.isEmpty()) {
-            return response.errorResponse(res, result.array()[0].msg);
-        } else {
-            let { group_type, group_name, arabic_group_name } = req.body
-            let data = {
-                group_type: group_type,
-                group_name: group_name,
-                arabic_group_name: arabic_group_name,
-                restaurant_id: req.currentUser
+        try {
+            const result = validationResult(req);
+            if (!result.isEmpty()) {
+                return response.errorResponse(res, result.array()[0].msg);
+            } else {
+                let { group_type, group_name, arabic_group_name } = req.body
+                let data = {
+                    group_type: group_type,
+                    group_name: group_name,
+                    arabic_group_name: arabic_group_name,
+                    restaurant_id: req.currentUser
+                }
+                const v = await QRGROUP.insertMany([data]);
+                return response.successResponse(res, message.ADD_OGROUP_SUCCESSFULLY);
             }
-            const v = await QRGROUP.insertMany([data]);
-            return response.successResponse(res, message.ADD_OGROUP_SUCCESSFULLY);
+        } catch {
+            response.errorResponse(res, message.INTERNAL_ERROR)
         }
     }
 ]
--- controller/qrCodes_controller.js
+++ controller/qrCodes_controller.js
@@ -41,12 +41,6 @@ exports.getqrcode = async (req, res) => {
 
 };
 
-exports.generateQR=async(req,res)=>{
-    try{
-        let id=req.params.id
-        let QR=
-    }
-}
 
 exports.getqrcodeById = async (req, res) => {
     const id = req.params.id;
@@ -89,7 +83,8 @@ exports.updateqrcode = [
                 return response.errorResponse(res, message.DATA_NOT_FOUND)
             }
         }
-    }];
+    }
+];
 
 exports.changeqrcodestatus = async (req, res) => {
     let id = req.params.id;
